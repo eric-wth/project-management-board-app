229e82db47015ae5182e1b82e6d65c65da615565
eric-waithaka <eric.email>
2022-07-28 12:34:17 +0300

    First commit

---
+++ src/hooks/useDataFetching.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+function useDataFetching(dataSource) {
+    const [loading, setLoading] = useState(false);    
+    const [error, setError] = useState('');
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await fetch(dataSource);
+
+                const result = await data.json();
+
+                if(result) {
+                    setData(result);
+                    setLoading(false);
+                }
+            }
+
+            catch (e) {
+                setLoading(false);
+                setError(e.message);
+            }
+            
+        }
+
+        fetchData();
+    }, [dataSource]);
+
+    return [loading, error, data];
+}
+
+export default useDataFetching;
